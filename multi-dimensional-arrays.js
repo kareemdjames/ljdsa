341f7a2d6a3f10d1ec0d33aa75777c131e598bad
Kareem James <kareemdjames@gmail.com>
2018-08-27 01:43:55 -0400

    messing around with multidirectional arrays

---
+++ multi-dimensional-arrays.js
@@ -0,0 +1,35 @@
+let averageTemp = [];
+
+// averageTemp[0] = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+averageTemp[0] = [72, 75, 79, 79, 81, 81, 90]
+averageTemp[1] = [81, 79, 75, 75, 73, 73, 87]
+
+function printMatrix(myMatrix) {
+  for (let i = 0; i < myMatrix.length; i++) {
+    for (let j = 0; j < myMatrix[i].length; j++) {
+      console.log(myMatrix[i][j])
+    }
+  }
+}
+
+// printMatrix(averageTemp)
+
+const matrix3x3x3 = [];
+
+for (let i = 0; i < 3; i++) {
+  matrix3x3x3[i] = []
+  for (let j = 0; j < 3; j++) {
+    matrix3x3x3[i][j] = [];
+    for (let z = 0; z < 3; z++) {
+      matrix3x3x3[i][j][z] = i + j + z
+    }
+  }
+}
+
+for (let i = 0; i < matrix3x3x3.length; i++) { 
+  for (let j = 0; j < matrix3x3x3[i].length; j++) { 
+    for (let z = 0; z < matrix3x3x3[i][j].length; z++) { 
+      console.log(matrix3x3x3[i][j][z]); 
+    } 
+  } 
+}
